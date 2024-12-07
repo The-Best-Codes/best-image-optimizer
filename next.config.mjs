bf691fbcd55370d925a8f8888d28db1da668cbd8
The-Best-Codes <bestcodes.official@gmail.com>
2024-12-07 11:03:37 -0600

    Update deps, use turbo builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true
+    },
+    eslint: {
+        ignoreDuringBuilds: true
+    },
+    experimental: {
+        turbo: {
+            useSwcCss: true,
+            treeShaking: true
+        }
+    }
+};
 
 export default nextConfig;
